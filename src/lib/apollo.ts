fde5e39e80710dfc229d6bf89d504321854c7e80
Lucas Barbosa Cesar de Oliveira <lucascoliveira819@gmail.com>
2022-06-26 01:42:54 -0400

    We made the Subscribe.tsx component, we made use of a mutation to create data in the api in graphQL in graphqlcms

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -2,7 +2,10 @@ import { ApolloClient, InMemoryCache } from "@apollo/client"
 
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4of6sta27el01z26iaza0b0/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 
 })
